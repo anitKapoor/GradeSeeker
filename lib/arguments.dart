e82c326b81a9d6d80135f9041756cbfb7b94d70b
Panav Munshi <panav.munshi@gmail.com>
2021-08-03 14:43:23 -0700

    Update arguments.dart

--- lib/arguments.dart
+++ lib/arguments.dart
@@ -1,6 +1,7 @@
 class UserArgs {
   final String userID;
   final bool isGuest;
+  final bool isLoginSuccessful;
 
-  UserArgs(this.userID, this.isGuest);
+  UserArgs(this.userID, this.isGuest, this.isLoginSuccessful);
 }
